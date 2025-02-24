5acd6673751168f4687f3aca76d308a92f571064
Yerimar <yeritachan@gmail.com>
2025-02-24 13:33:06 -0400

    Quitando esquelto de funcion de traduccion emoji del main

--- src/main.cpp
+++ src/main.cpp
@@ -4,15 +4,6 @@
 
 extern FILE* yyin;
 
-/*
-char* emojiTranslator(int ascii){
-    // Idea de funcion a completar:
-        // Leer archivo completo
-        // Sustituir por nombre clave ("nubecita", "manito", por ejemplo)
-        // Guardar codigo en archivo temporal
-}
-*/
-
 int main(int argc, char** argv) {
     if (argc < 2) {
         std::cerr << "Uso: " << argv[0] << " <archivo de entrada>" << std::endl;
